969f3fa2cb7701f2a0777374fdd15774d6857c22
linuxnep <86525017+LinusNEP@users.noreply.github.com>
2026-03-26 11:32:50 +0100

    Update links to point to GitHub repository

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -409,7 +409,7 @@ set_velocity(pitch, roll)`
               <Button 
                 size="sm" 
                 className="bg-[#d0ff59] text-black hover:bg-[#b8e650] font-medium"
-                onClick={() => window.open('https://doi.org/10.17605/OSF.IO/K83X7', '_blank')}
+                onClick={() => window.open('https://github.com/LinusNEP/ROS-Mobile-Robot', '_blank')}
               >
                 <Download className="w-4 h-4 mr-2" />
                 Download
@@ -493,7 +493,7 @@ set_velocity(pitch, roll)`
                 <Button 
                   size="lg" 
                   className="bg-[#d0ff59] text-black hover:bg-[#b8e650] font-semibold"
-                  onClick={() => window.open('https://doi.org/10.17605/OSF.IO/K83X7', '_blank')}
+                  onClick={() => window.open('https://github.com/LinusNEP/ROS-Mobile-Robot', '_blank')}
                 >
                   <Download className="w-5 h-5 mr-2" />
                   Get Started
@@ -1099,7 +1099,7 @@ set_velocity(pitch, roll)`
             <Button 
               size="lg" 
               className="bg-[#d0ff59] text-black hover:bg-[#b8e650] font-semibold text-lg px-8"
-              onClick={() => window.open('https://doi.org/10.17605/OSF.IO/K83X7', '_blank')}
+              onClick={() => window.open('https://github.com/LinusNEP/ROS-Mobile-Robot', '_blank')}
             >
               <Download className="w-5 h-5 mr-2" />
               Download Files
@@ -1151,7 +1151,7 @@ set_velocity(pitch, roll)`
             <div>
               <h4 className="font-semibold mb-4">Resources</h4>
               <ul className="space-y-2 text-sm text-[#666666]">
-                <li><a href="https://doi.org/10.17605/OSF.IO/K83X7" className="hover:text-[#d0ff59] transition-colors">Design Files</a></li>
+                <li><a href="https://github.com/LinusNEP/ROS-Mobile-Robot" className="hover:text-[#d0ff59] transition-colors">Design Files</a></li>
                 <li><a href="https://osf.io/ku8ag" className="hover:text-[#d0ff59] transition-colors">Demo Video</a></li>
                 <li><a href="#" className="hover:text-[#d0ff59] transition-colors">Documentation</a></li>
               </ul>
